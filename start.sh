abc37973cf9926a6f3f4f43ced300518e89bf37f
Moses Okemwa <okemwamoses@gmail.com>
2019-09-23 13:42:10 +0300

    dev server

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 export APPLICATION_MODE=config.ProductionConfig
 flask db upgrade
-gunicorn -b 0.0.0.0:5000 hardcover:application
+# gunicorn -b 0.0.0.0:5000 hardcover:application
+flask run -h 0.0.0.0 -p 5000
